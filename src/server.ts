390e4ff8bc6341ee250c682196d8265ec69e8d76
. <fadelnajmiadliansyah@gmail.com>
2026-04-18 17:08:32 +0700

    fix/cors-policy

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://konco-server.vercel.app"],
+    origin: ["http://localhost:5173","https://konco-client.vercel.app"],
     credentials: true,
   }),
 );
